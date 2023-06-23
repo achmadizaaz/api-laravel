1fba6e52f5b10cb534e1504c5ac577d5057f7a3c
AchmadIzaaz <myachmadizaaz@gmail.com>
2023-06-23 19:12:11 +0700

    Add Factory and Seeder Post

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'text' => $this->faker->sentence(15),
+            'author' => $this->faker->sentence(3)
+        ];
+    }
+}
